8eb4c535b5e9d526e17609ead4ed3b6ec533c71e
egemen-dev <egemenwasd@gmail.com>
2023-05-27 23:08:08 +0300

    Add project features as bulletpoints to index page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, world!</h1>
+      <ul>
+        <li>A section to add general information like name, email, phone number.</li>
+        <li>
+          A section to add your educational experience (school name, title of study, date
+          of study)
+        </li>
+        <li>
+          A section to add practical experience (company name, position title, main tasks
+          of your jobs, date from and until when you worked for that company)
+        </li>
+        <li>A button to download your CV in PDF format</li>
+        <li>A button to reset the CV</li>
+      </ul>
     </div>
   );
 }
