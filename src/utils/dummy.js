4187bf9f9c001ebc174bfdad82f3743ad7f94986
egemen-dev <egemenwasd@gmail.com>
2023-05-30 12:17:48 +0300

    Create dummy file for repeated texts

---
+++ src/utils/dummy.js
@@ -0,0 +1,23 @@
+const dummy = {
+  name: "John Doe",
+  mail: "example@mail.com",
+  phone: "0123456789",
+  website: "linkedin / github",
+  skills: "JavaScript, React, HTML, CSS",
+  work: {
+    title: "Google",
+    subTitle: "Software Engineer",
+    details: "Developing software",
+    from: "2020-01-01",
+    until: "2021-01-01",
+  },
+  education: {
+    title: "University of Life",
+    subTitle: "Bachelor of Science",
+    details: "Studying",
+    from: "2020-01-01",
+    until: "2021-01-01",
+  },
+};
+
+export default dummy;
