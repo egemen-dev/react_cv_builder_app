6cb3ccee6be451dc495d80e516e84fa9e7a6507f
egemen-dev <egemenwasd@gmail.com>
2023-05-30 12:21:47 +0300

    Refactor app js for readability

--- src/App.js
+++ src/App.js
@@ -1,116 +1,128 @@
 import "./styles/style.css";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import Textarea from "./components/Textarea";
-import CvHeader from "./components/cv/Header";
-import Section from "./components/cv/Section";
-import CollapsibleCard from "./components/CollapsibleCard";
-import WorkInputSection from "./components/WorkInputSection";
-import EducationInputSection from "./components/EducationInputSection";
 import { useState } from "react";
+import dummy from "./utils/dummy";
+import Input from "./components/form/Input";
+import Button from "./components/form/Button";
+import Textarea from "./components/form/Textarea";
+import Section from "./components/preview/Section";
+import CvHeader from "./components/preview/Header";
+import InputSection from "./components/form/InputSection";
+import CollapsibleCard from "./components/form/CollapsibleCard";
 
 function App() {
-  const [workSections, setWorkSections] = useState([]);
-  const [educationSections, setEducationSections] = useState([]);
-  const [name, setName] = useState("John Doe");
-  const [mail, setMail] = useState("example@mail.com");
-  const [phone, setPhone] = useState("0123456789");
-  const [website, setWebsite] = useState("linkedin / github");
-  const [skills, setSkills] = useState("JavaScript, React, HTML, CSS");
+  const [workSection, setWorkSection] = useState([]);
+  const [educationSection, setEducationSection] = useState([]);
+  const [name, setName] = useState(dummy.name);
+  const [mail, setMail] = useState(dummy.mail);
+  const [phone, setPhone] = useState(dummy.phone);
+  const [website, setWebsite] = useState(dummy.website);
+  const [projects, setProjects] = useState(dummy.projects);
+  const [skills, setSkills] = useState(dummy.skills);
 
-  const baseSection = {
-    title: "Title",
-    subTitle: "SubTitle",
-    details: "Details",
-    from: "2020-01-01",
-    until: "2020-01-01",
+  const baseWorkSection = {
+    title: dummy.work.title,
+    subTitle: dummy.work.subTitle,
+    details: dummy.work.details,
+    from: dummy.work.from,
+    until: dummy.work.until,
+  };
+
+  const baseEducationSection = {
+    title: dummy.education.title,
+    subTitle: dummy.education.subTitle,
+    details: dummy.education.details,
+    from: dummy.education.from,
+    until: dummy.education.until,
   };
 
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-11/12 md:w-3/4 mx-auto py-6">
-        <div className="flex flex-col gap-4 non-printable">
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-11/12 md:w-3/4 py-6 h-screen container gap-6 mx-auto">
+        <div className="flex flex-col gap-5 non-printable place-self-center">
           <CollapsibleCard title="General Info">
             <Input
               label="Name"
-              placeholder="John Doe"
+              placeholder={dummy.name}
               onChange={(e) => setName(e.target.value)}
             />
             <Input
               label="Email"
-              placeholder="mail@example.com"
+              placeholder={dummy.mail}
               onChange={(e) => setMail(e.target.value)}
             />
             <Input
               label="Phone number"
-              type="tel"
-              placeholder="0123456789"
+              placeholder={dummy.phone}
               onChange={(e) => {
                 setPhone(e.target.value);
               }}
             />
             <Input
               label="Website"
-              placeholder="linkedin / github"
+              placeholder={dummy.website}
               onChange={(e) => setWebsite(e.target.value)}
             />
           </CollapsibleCard>
 
           <CollapsibleCard title="Education">
-            {educationSections.map((_item, index) => (
-              <>
-                <hr className="col-span-2 border-b-1 my-4 border-black dark:border-white" />
-                <EducationInputSection
-                  index={index}
-                  educationSections={educationSections}
-                  setEducationSections={setEducationSections}
-                />
-              </>
+            {educationSection.map((_item, index) => (
+              <InputSection
+                index={index}
+                sectionName="education"
+                educationSection={educationSection} // getter state function
+                setEducationSection={setEducationSection} // setter state function
+              />
             ))}
 
-            <Button
-              label="Add More"
-              sectionName="education"
-              onClick={() => setEducationSections([...educationSections, baseSection])}
-            />
-            {educationSections.length > 1 && (
+            {educationSection.length > 0 && (
               <Button
                 label="Remove"
                 sectionName="education"
-                onClick={() => setEducationSections(educationSections.slice(0, -1))}
+                className="btn btn-outline btn-outline col-span-full"
+                onClick={() => setEducationSection(educationSection.slice(0, -1))}
+              />
+            )}
+            {educationSection.length < 4 && (
+              <Button
+                label="Add More"
+                sectionName="education"
+                onClick={() =>
+                  setEducationSection([...educationSection, baseEducationSection])
+                }
               />
             )}
           </CollapsibleCard>
 
           <CollapsibleCard title="Work Experience">
-            {workSections.map((_item, index) => (
-              <>
-                <hr className="col-span-2 border-b-1 my-4 border-black dark:border-white" />
-                <WorkInputSection
-                  index={index}
-                  workSections={workSections}
-                  setWorkSections={setWorkSections}
-                />
-              </>
+            {workSection.map((_item, index) => (
+              <InputSection
+                index={index}
+                sectionName="work"
+                workSection={workSection} // getter state function
+                setWorkSection={setWorkSection} // setter state function
+              />
             ))}
 
-            <Button
-              label="Add More"
-              sectionName="work"
-              onClick={() => setWorkSections([...workSections, baseSection])}
-            />
-            {workSections > 1 && (
+            {workSection.length > 0 && (
               <Button
                 label="Remove"
                 sectionName="work"
-                onClick={() => setWorkSections(workSections.slice(0, -1))}
+                className="btn btn-outline btn-outline col-span-full"
+                onClick={() => setWorkSection(workSection.slice(0, -1))}
+              />
+            )}
+            {workSection.length < 4 && (
+              <Button
+                label="Add More"
+                sectionName="work"
+                onClick={() => setWorkSection([...workSection, baseWorkSection])}
               />
             )}
           </CollapsibleCard>
 
-          <CollapsibleCard title="Skills">
+          <CollapsibleCard title="Projects & Skills">
             <Textarea
-              placeholder="HTML, CSS, JavaScript, Ruby, React"
+              placeholder={dummy.skills}
               onChange={(e) => setSkills(e.target.value)}
             />
           </CollapsibleCard>
@@ -128,44 +140,47 @@ function App() {
 
         {/* Downloadable CV Starts here */}
 
-        <div className="py-2 grid grid-cols-1 w-full gap-6 min-h-content max-h-content printable">
-          <CvHeader name={name} mail={mail} phone={phone} website={website} />
-          <section>
-            <h2 className="text-3xl">Education</h2>
+        <div className="grid grid-cols-1 w-full gap-6 min-h-content max-h-content printable border-2 border-gray-300 dark:border-gray-600 p-6">
+          <div className="flex flex-col justify-start">
+            <CvHeader name={name} mail={mail} phone={phone} website={website} />
+            <hr className="border-b-2.5 border-gray-400 dark:border-gray-600" />
+            <section>
+              <h2 className="text-3xl py-2">Education</h2>
 
-            {educationSections.map((state, index) => (
-              <Section
-                key={index}
-                title={state.title}
-                subTitle={state.subTitle}
-                details={state.details}
-                from={state.from}
-                until={state.until}
-              />
-            ))}
-          </section>
-          <hr className="border-b-2 border-black dark:border-white" />
-          <section>
-            <h2 className="text-3xl pt-3">Work Experience</h2>
+              {educationSection.map((state, index) => (
+                <Section
+                  key={index}
+                  title={state.title}
+                  subTitle={state.subTitle}
+                  details={state.details}
+                  from={state.from}
+                  until={state.until}
+                />
+              ))}
+            </section>
+            <hr className="border-b-2.5 border-gray-400 dark:border-gray-600" />
+            <section>
+              <h2 className="text-3xl py-2">Work Experience</h2>
 
-            {workSections.map((state, index) => (
-              <Section
-                key={index}
-                title={state.title}
-                subTitle={state.subTitle}
-                details={state.details}
-                from={state.from}
-                until={state.until}
-              />
-            ))}
-          </section>
-          <hr className="border-b-2 border-black dark:border-white" />
-          <section>
-            <h2 className="text-3xl py-3">Skills</h2>
-            <div className="flex flex-col gap-3">
-              <p>{skills}</p>
-            </div>
-          </section>
+              {workSection.map((state, index) => (
+                <Section
+                  key={index}
+                  title={state.title}
+                  subTitle={state.subTitle}
+                  details={state.details}
+                  from={state.from}
+                  until={state.until}
+                />
+              ))}
+            </section>
+            <hr className="border-b-2.5 border-gray-400 dark:border-gray-600" />
+            <section>
+              <h2 className="text-3xl py-2">Projects & Skills</h2>
+              <div className="flex flex-col gap-3">
+                <p>{skills}</p>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </div>
