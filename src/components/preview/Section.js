df8358df6b4032df69dd047d09f5f13f72a296e8
egemen-dev <egemenwasd@gmail.com>
2023-06-01 00:06:24 +0300

    Refator preview section component

--- src/components/preview/Section.js
+++ src/components/preview/Section.js
@@ -1,16 +1,22 @@
-export default function Section({ title, subTitle, details, from, until }) {
+export default function Section({ index, experience }) {
   return (
-    <div className="flex flex-col pb-5 pt-2">
+    <div key={index} className="flex flex-col pb-5 pt-2">
       <div className="flex justify-between">
-        <p className="font-bold">{title}</p>
+        <p className="font-bold">{experience.title}</p>
         <div className="flex gap-3">
-          <p>{until}</p>
-          <p>{from}</p>
+          <p>{experience.until}</p>
+          <p>{experience.from}</p>
         </div>
       </div>
       <div className="flex flex-col gap-3">
-        <p>{subTitle}</p>
-        <p>{details}</p>
+        <p>{experience.subTitle}</p>
+        <p>{experience.details}</p>
+        {experience.bulletPoints.map((bulletPoint, bulletPointIndex) => (
+          <div key={bulletPointIndex} className="flex gap-3">
+            <p className="font-bold">•</p>
+            <p>{bulletPoint}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
