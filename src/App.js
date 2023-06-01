f0c2c6e7416521058da2ff9803c0e230766b99ff
egemen-dev <egemenwasd@gmail.com>
2023-06-01 22:00:21 +0300

    Create states and pass it down to components as props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/style.css";
 import { useState } from "react";
-import dummy from "./utils/dummy";
+import dummy from "./utils/dummy.json";
 import Input from "./components/form/Input";
 import Button from "./components/form/Button";
 import Textarea from "./components/form/Textarea";
@@ -10,10 +10,22 @@ import InputSection from "./components/form/InputSection";
 import CollapsibleCard from "./components/form/CollapsibleCard";
 
 function App() {
-  const dummyExample = { ...dummy };
-  const [workSection, setWorkSection] = useState([]);
-  const [educationSection, setEducationSection] = useState([]);
-  const [cv, setCv] = useState(dummyExample);
+  const [name, setName] = useState(dummy.name);
+  const [mail, setMail] = useState(dummy.mail);
+  const [phone, setPhone] = useState(dummy.phone);
+  const [website, setWebsite] = useState(dummy.website);
+  const [educations, setEducations] = useState(dummy.educations);
+  const [works, setWorks] = useState(dummy.works);
+  const [skills, setSkills] = useState(dummy.projects);
+
+  const [experience, setExperience] = useState({
+    from: "01/01/2020",
+    until: "01/01/2021",
+    title: "Lorem Ipsum",
+    subTitle: "Lorem Ipsum Dolor",
+    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bulletPoints: ["Very good at stuff"],
+  });
 
   return (
     <div>
@@ -23,81 +35,77 @@ function App() {
             <Input
               label="Name"
               placeholder={dummy.name}
-              onChange={(e) => setCv({ ...cv, name: e.target.value })}
+              onChange={(e) => setName(e.target.value)}
             />
             <Input
               label="Email"
               placeholder={dummy.mail}
-              onChange={(e) => setCv({ ...cv, mail: e.target.value })}
+              onChange={(e) => setMail(e.target.value)}
             />
             <Input
               label="Phone number"
               placeholder={dummy.phone}
-              onChange={(e) => {
-                setCv({ ...cv, phone: e.target.value });
-              }}
+              onChange={(e) => setPhone(e.target.value)}
             />
             <Input
               label="Website"
               placeholder={dummy.website}
-              onChange={(e) => setCv({ ...cv, website: e.target.value })}
+              onChange={(e) => setWebsite(e.target.value)}
             />
           </CollapsibleCard>
 
           <CollapsibleCard title="Education">
-            {cv.education.map((_item, index) => (
+            {educations.map((item, index) => (
               <InputSection
+                sectionName="educations"
                 index={index}
-                sectionName="education"
-                // educationSection={educationSection} // getter state function
-                // setEducationSection={setEducationSection} // setter state function
+                educations={educations}
+                setEducations={setEducations}
               />
             ))}
-
-            {cv.education.length > 0 && (
-              <Button
-                label="Remove Education"
-                sectionName="education"
-                className="btn btn-outline btn-outline col-span-full"
-                // onClick={() => setEducationSection([...cv.education].slice(0, -1))}
-              />
-            )}
-            {educationSection.length < 4 && (
-              <Button
-                label="Add More Education"
-                sectionName="education"
-                onClick={() => {
-                  // setEducationSection(...cv.education, dummyExample.education[0]);
-                }}
-              />
-            )}
+            <div className="flex flex-row gap-2 py-8 col-span-full">
+              {educations.length > 0 && (
+                <Button
+                  label="Remove Education"
+                  sectionName="education"
+                  className="btn btn-outline btn-outline flex-grow"
+                  onClick={() => setEducations([...educations].slice(0, -1))}
+                />
+              )}
+              {educations.length < 3 && (
+                <Button
+                  label="Add More Education"
+                  sectionName="educations"
+                  onClick={() => {
+                    setEducations([...educations, { ...experience }]);
+                  }}
+                />
+              )}
+            </div>
           </CollapsibleCard>
 
           <CollapsibleCard title="Work Experience">
-            {workSection.map((_item, index) => (
+            {works.map((item, index) => (
               <InputSection
                 index={index}
-                sectionName="work"
-                // workSection={workSection} // getter state function
-                // setWorkSection={setWorkSection} // setter state function
+                sectionName="works"
+                works={works}
+                setWorks={setWorks}
               />
             ))}
-
-            {workSection.length > 0 && (
+            {works.length > 0 && (
               <Button
                 label="Remove Experience"
-                sectionName="work"
+                sectionName="works"
                 className="btn btn-outline btn-outline col-span-full"
-                // onClick={() => setWorkSection([...workSection].slice(0, -1))}
+                onClick={() => setWorks([...works].slice(0, -1))}
               />
             )}
-            {workSection.length < 4 && (
+            {works.length < 3 && (
               <Button
                 label="Add More Experience"
-                sectionName="work"
-                onClick={() => {
-                  // setWorkSection([...workSection, cv.work[0]]);
-                }}
+                sectionName="works"
+                onClick={() => setWorks([...works, { ...experience }])}
               />
             )}
           </CollapsibleCard>
@@ -105,7 +113,7 @@ function App() {
           <CollapsibleCard title="Projects & Skills">
             <Textarea
               placeholder={dummy.skills}
-              onChange={(e) => setCv({ ...cv, skills: e.target.value })}
+              onChange={(e) => setSkills(e.target.value)}
             />
           </CollapsibleCard>
 
@@ -124,17 +132,12 @@ function App() {
 
         <div className="grid grid-cols-1 w-full gap-6 min-h-content max-h-content printable border-2 border-gray-300 dark:border-gray-600 p-6">
           <div className="flex flex-col justify-start">
-            <CvHeader
-              name={cv.name}
-              mail={cv.mail}
-              phone={cv.phone}
-              website={cv.website}
-            />
+            <CvHeader name={name} mail={mail} phone={phone} website={website} />
             <section>
               <h2 className="text-3xl py-2">Education</h2>
               <hr className="border-b-2.5 border-gray-400 dark:border-gray-600" />
 
-              {cv.education.map((education, index) => (
+              {educations.map((education, index) => (
                 <Section index={index} experience={education} />
               ))}
             </section>
@@ -142,7 +145,7 @@ function App() {
               <h2 className="text-3xl py-2">Work Experience</h2>
               <hr className="border-b-2.5 border-gray-400 dark:border-gray-600" />
 
-              {workSection.map((work, index) => (
+              {works.map((work, index) => (
                 <Section index={index} experience={work} />
               ))}
             </section>
@@ -150,7 +153,7 @@ function App() {
               <h2 className="text-3xl py-2">Projects & Skills</h2>
               <hr className="border-b-2.5 border-gray-400 dark:border-gray-600" />
               <div className="flex flex-col gap-3">
-                <p>{cv.skills}</p>
+                <p>{skills}</p>
               </div>
             </section>
           </div>
