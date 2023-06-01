00dfbe985602d250b53a2e8930661e1efa887234
egemen-dev <egemenwasd@gmail.com>
2023-06-01 21:56:41 +0300

    Refactor input component

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,20 +1,20 @@
-export default function Input({
-  index = 0,
-  label,
-  placeholder,
-  type,
-  onChange,
-  className,
-}) {
+export default function Input({ index = 0, label, placeholder, type, onChange }) {
   return (
-    <div key={index} className={className || "form-control col-span-1 w-full"}>
-      <label className="label">
-        <span className="label-text">{label}</span>
-      </label>
+    <div
+      key={index}
+      className={
+        "form-control col-span-1 w-full border-b-2 border-gray-200 dark:border-gray-600 p-0.5 "
+      }
+    >
+      {label && (
+        <label className="label">
+          <span className="label-text">{label}</span>
+        </label>
+      )}
       <input
         type={type}
         placeholder={placeholder}
-        className="input input-bordered w-full"
+        className="input w-full"
         onChange={onChange}
       />
     </div>
